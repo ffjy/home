6f2c35e49321c76870d9ed680111a7dcc0848310
maczyt <369280416@qq.com>
2017-10-23 14:28:53 +0800

    学员列表和老师列表

---
+++ src/js/common/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#back').on('click', function() {
+    window.history.back()
+  })
+});
--- src/js/students/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('#back').on('click', function() {
-    window.history.back()
-  })
-});
