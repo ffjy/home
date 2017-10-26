3703a84abd24b72e8e83161cac01387a8f76b6fd
maczyt <369280416@qq.com>
2017-10-26 09:41:35 +0800

    增加fastclick

--- src/js/common/index.js
+++ src/js/common/index.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
-  $('#back').on('click', function() {
-    window.history.back()
-  })
-});
+    /**
+     * 返回history back
+     */
+    $('#back').on('click', function() {
+        window.history.back()
+    });
 
-$(document).ready(function() {
-    var mySwiper = $('.swiper-container').swiper({
-        slidesPerView: 'auto',
-        freeMode: true,
-        freeModeMomentumRatio: 0.5,
-        slidesPerView: 'auto',
-    }).on('touchstart', function(e) {
-        e.preventDefault();
-    })
+    /**
+     * nav切换
+     */
+    if ($('.swiper-container').length) {
+        var mySwiper = $('.swiper-container').swiper({
+            slidesPerView: 'auto',
+            freeMode: true,
+            freeModeMomentumRatio: 0.5,
+            slidesPerView: 'auto',
+        }).on('touchstart', function(e) {
+            e.preventDefault();
+        })
 
-    mySwiper.on('tap', function(swiper, e) {
-        $(".swiper-container  .active").removeClass('active')
-        $(".swiper-container .swiper-slide").eq(swiper.clickedIndex).addClass('active')
-    })
+        mySwiper.on('tap', function(swiper, e) {
+            $(".swiper-container  .active").removeClass('active')
+            $(".swiper-container .swiper-slide").eq(swiper.clickedIndex).addClass('active')
+        })
+    }
+
+    /**
+     * fastclick
+     */
+    FastClick.attach(document.body);
 });
